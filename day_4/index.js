2b1f01557dd8d26402d1cb765ac5b2006590ac69
fez <fez@thoughtmachine.net>
2023-12-22 10:52:36 +0000

    part 2

--- day_4/index.js
+++ day_4/index.js
@@ -6,31 +6,53 @@ fs.readFile("input.txt", "utf8", (err, data) => {
     return;
   }
 
-  const { solution } = accumulateData(data.split("\n"));
+  const { solution, totalCards } = accumulateData(data.split("\n"));
 
-  console.log("====", solution);
+  console.log("----", solution);
+  console.log("====", totalCards);
 });
 
 const accumulateData = (games) => {
   let solution = 0;
 
-  games.forEach((game) => {
+  const cardCount = games.reduce(
+    (acc, _, i) => ({
+      ...acc,
+      [i + 1]: 1,
+    }),
+    {}
+  );
+
+  games.forEach((game, i) => {
+    const currentGame = i + 1;
     const numbersInPlay = game.split(": ")[1].split(" | ");
     const winningNums = numbersInPlay[0].split(" ").filter(Boolean);
     const elfsNums = numbersInPlay[1].split(" ").filter(Boolean);
-    
-    let wins = 0
+
+    let wins = 0;
     elfsNums.forEach((num) => {
       if (winningNums.includes(num)) {
-        wins++
+        wins++;
       }
     });
 
-    if(wins > 0){
-      solution += Math.pow(2, wins-1)
+    if (wins > 0) {
+      solution += Math.pow(2, wins - 1);
     }
-   
+
+    for (let idx = 1; idx <= wins; idx++) {
+      const gameToAddTo = currentGame + idx;
+      const numbOfCardsForGame = cardCount?.[currentGame];
+      if (cardCount?.[gameToAddTo]) {
+        cardCount[gameToAddTo] += numbOfCardsForGame;
+      }
+    }
   });
 
-  return { solution };
+  const totalCards = Object.values(cardCount).reduce(
+    (acc, count) => (acc += count),
+    0
+  );
+
+  return { solution, totalCards };
 };
