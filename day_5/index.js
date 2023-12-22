3ec60061c03164be699dea59421c010bb9b14fe9
Faiz Abbas <43446772+Faizerz@users.noreply.github.com>
2023-12-22 14:53:46 +0000

    meowth

--- day_5/index.js
+++ day_5/index.js
@@ -26,16 +26,15 @@ const getLowestLocation = (data) => {
   } = getFormattedMaps(dataMaps);
 
   for (let i = 0; i < seeds.length; i++) {
-    const seed = seeds[i];
-    const soil = seedToSoil?.[seed] || seed;
-    const fertilizer = soilToFertalizer[soil] || soil;
-    const water = fertalizerToWater[fertilizer] || fertilizer;
-    const light = waterToLight[water] || water;
-    const temperature = lightToTemperature[light] || light;
-    const humidity = temperatureToHumidity[temperature] || temperature;
-    const location = humidityToLocation[humidity] || humidity;
+    const seed = Number(seeds[i]);
+    const soil = getValueFromMap(seed, seedToSoil) || seed;
+    const fertilizer = getValueFromMap(soil, soilToFertalizer) || soil;
+    const water = getValueFromMap(fertilizer, fertalizerToWater);
+    const light = getValueFromMap(water, waterToLight);
+    const temperature = getValueFromMap(light, lightToTemperature);
+    const humidity = getValueFromMap(temperature, temperatureToHumidity);
+    const location = getValueFromMap(humidity, humidityToLocation);
 
-    console.log(location);
     if (location < lowestLocation || lowestLocation === null) {
       lowestLocation = location;
     }
@@ -68,6 +67,7 @@ const getDataMaps = (data) => {
 
 const getFormattedMaps = (dataMaps) => {
   const maps = {};
+
   for (const [key, value] of Object.entries(dataMaps)) {
     maps[key] = value.reduce((acc, row) => {
       const splitRow = row.split(" ");
@@ -75,14 +75,54 @@ const getFormattedMaps = (dataMaps) => {
       const seedRange = Number(splitRow[1]);
       const rangeLength = Number(splitRow[2]);
 
-      for (let i = 0; i < rangeLength; i++) {
-        acc = {
-          ...acc,
-          [seedRange + i]: soilRange + i,
-        };
-      }
-      return acc;
-    }, {});
+      const minOfRage = seedRange;
+      const maxOfRange = seedRange + rangeLength;
+      const difference = soilRange - seedRange;
+
+      return [...acc, [minOfRage, maxOfRange, difference]];
+    }, []);
   }
   return maps;
 };
+
+const getValueFromMap = (x, rangeArray) => {
+  let value = null;
+
+  rangeArray.find((rangeItem) => {
+    const min = Number(rangeItem[0]);
+    const max = Number(rangeItem[1]);
+    const difference = Number(rangeItem[2]);
+
+    if (min <= x <= max) {
+      value = Number(x) + difference;
+      return;
+    }
+  });
+
+  return value;
+};
+
+// if (50 >= x <= 97) {
+//   return x + (soilRange - seedRange);
+// }
+
+// const getFormattedMaps = (dataMaps) => {
+//   const maps = {};
+//   for (const [key, value] of Object.entries(dataMaps)) {
+//     maps[key] = value.reduce((acc, row) => {
+//       const splitRow = row.split(" ");
+//       const soilRange = Number(splitRow[0]);
+//       const seedRange = Number(splitRow[1]);
+//       const rangeLength = Number(splitRow[2]);
+
+//       for (let i = 0; i < rangeLength; i++) {
+//         acc = {
+//           ...acc,
+//           [seedRange + i]: soilRange + i,
+//         };
+//       }
+//       return acc;
+//     }, {});
+//   }
+//   return maps;
+// };
