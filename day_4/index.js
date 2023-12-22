053d44a0a0a18659fb6f94db7d308e240a7375e3
fez <fez@thoughtmachine.net>
2023-12-22 10:36:12 +0000

    part 1

--- day_4/index.js
+++ day_4/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.readFile("test.txt", "utf8", (err, data) => {
+fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) {
     console.error("Error reading file:", err);
     return;
@@ -18,23 +18,18 @@ const accumulateData = (games) => {
     const numbersInPlay = game.split(": ")[1].split(" | ");
     const winningNums = numbersInPlay[0].split(" ").filter(Boolean);
     const elfsNums = numbersInPlay[1].split(" ").filter(Boolean);
+    
+    let wins = 0
+    elfsNums.forEach((num) => {
+      if (winningNums.includes(num)) {
+        wins++
+      }
+    });
 
-    const pointsMap = winningNums.reduce(
-      (acc, num, i) => ({
-        ...acc,
-        [num]: i,
-      }),
-      {}
-    );
-
-    console.log(elfsNums);
-
-    const gameTotal = elfsNums.reduce(
-      (acc, num) => (acc += pointsMap?.[num] ? pointsMap[num] : 0),
-      0
-    );
-    console.log(pointsMap);
-    console.log(gameTotal);
+    if(wins > 0){
+      solution += Math.pow(2, wins-1)
+    }
+   
   });
 
   return { solution };
